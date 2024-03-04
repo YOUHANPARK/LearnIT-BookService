c48964ab46dab1ea117866481eb95a27355e514a
UserJiwon <jwngo@naver.com>
2024-03-04 18:56:27 +0900

    0304 BookDto, BookService 수정

--- src/book/dto/BookDto.java
+++ src/book/dto/BookDto.java
@@ -1,15 +1,204 @@
 package book.dto;
 
+import java.util.Date;
+
 public class BookDto {
-
-	public static class SearchBookByTitleInput {
+	
+	public static class SearchBookByTitleInputDto{
 		String title;
-
+		
+	}
+	public static class SearchBookByTitleOutputDto {
+		long bookseq;
+		String title;
+		int booknum;
+		String publisher;
+		String author;
+		boolean bookposb;
 		
 		
 	}
 	
-	public static class SearchBookByTitleOutput {
+	public static class SearchBookByCategoryInputDto {
+		String category;
 		
 	}
+	public static class SearchBookByCategoryOutputDto {
+		long bookseq;
+		String title;
+		int booknum;
+		String publisher;
+		String author;
+		boolean bookposb;
+	}
+	
+	public static class SearchBookBySeqInputDto{
+		long bookseq;
+	}
+	public static class SearchBookBySeqOutputDto{
+		long bookseq;
+		String title;
+		int booknum;
+		String publisher;
+		String author;
+		String intro;
+		boolean bookposb;
+		int pubyear;
+	}
+	
+	public static class AddFavoriteBookInputDto{
+		long userseq;
+		long bookseq;
+		Date favordate;
+	}
+	
+	
+	public static class ViewFavoriteBookInputDto{
+		long userseq;
+	}
+	public static class ViewFavoriteBookOutputDto{
+		String title;
+		int booknum;
+		String publisher;
+		String author;
+		boolean bookposb;
+	}
+	
+	public static class DeleteFavoriteBookInputDto{
+		String title;
+		long userseq;
+	}
+	
+	public static class CheckBookAvailabilityBySeqInputDto{
+		long bookseq;
+		long userseq;
+	}
+	public static class CheckBookAvailabilityBySeqOutputDto{
+		Date returndate;
+		int bookposbnum;
+	}
+	
+	public static class RegisterBookUnrequestedInputDto{
+		String title;
+		int booknum;
+		String publisher;
+		String intro;
+		String author;
+		int pubyear;
+		boolean bookposb;
+		long userseq;
+	}
+	
+	public static class RegisterBookRequestedInputDto{
+		String title;
+		int booknum;
+		String publisher;
+		String intro;
+		String author;
+		int pubyear;
+		boolean bookposb;
+		long userseq;
+	}
+	
+	public static class UpdateBookBySeqInputDto{
+		String title;
+		int booknum;
+		String publisher;
+		String author;
+		int pubyear;
+		long bookseq;
+		long userseq;
+	}
+	
+	public static class DeleteBookInputDto{
+		long bookseq;
+		long userseq;
+	}
+	
+	public static class RequestBookInputDto{
+		String title;
+		String publisher;
+		String author;
+		int pubyear;
+		long userseq;
+	}
+	
+	public static class ViewBookRequestsInputDto{
+		String title;
+		String publisher;
+		String author;
+		int pubyear;
+		long userseq;
+	}
+	public static class ViewBookRequestsOutputDto{
+		Date requestdate;
+		int requeststatus;
+		String addtitle;
+		String addauthor;
+	}
+	
+	public static class ManageBookRequestInputDto{
+		String title;
+		String publisher;
+		String author;
+		int pubyear;
+		long userseq;
+	}
+	public static class ManageBookRequestOutputDto{
+		Date requestdate;
+		int requeststatus;
+		String addtitle;
+		String addauthor;
+		long requestuserseq;
+	}
+	
+	public static class ReturnBookBySeqInputDto{
+		long bookseq;
+		long userseq;
+	}
+	public static class ReturnBookBySeqOutputDto{
+		Date rentdate;
+		Date returndate;
+	}
+	
+	public static class ExtendBookLoanBySeqInputDto{
+		long bookseq;
+		long userseq;
+	}
+	public static class ExtendBookLoanBySeqOutputDto{
+		Date returndate;
+	}
+	
+	public static class RateBookInputDto{
+		int score;
+		long bookseq;
+		long userseq;
+	}
+	
+	public static class ViewLoanHistoryInputDto{
+		long userseq;
+	}
+	public static class ViewLoanHistoryOutputDto{
+		String title;
+		int booknum;
+		String publisher;
+		String author;
+		int pubyear;
+		Date loandate;
+		Date returndate;
+	}
+	
+	public static class RecommendBookInputDto{
+		long userseq;
+		long ratingseq;
+	}
+	public static class RecommendBookOutputDto{
+		String title;
+		int booknum;
+		String publisher;
+		String author;
+		int pubyear;
+		boolean bookposb;
+	}
 }
+
--- src/book/service/BookService.java
+++ src/book/service/BookService.java
@@ -1,11 +1,76 @@
 package book.service;
 
-import  book.dto.BookDto;
-import book.dto.BookDto.SearchBookByTitleInput;
-import book.dto.BookDto.SearchBookByTitleOutput;
+import java.util.List;
+
+import book.dto.BookDto.AddFavoriteBookInputDto;
+import book.dto.BookDto.CheckBookAvailabilityBySeqInputDto;
+import book.dto.BookDto.CheckBookAvailabilityBySeqOutputDto;
+import book.dto.BookDto.DeleteBookInputDto;
+import book.dto.BookDto.DeleteFavoriteBookInputDto;
+import book.dto.BookDto.ExtendBookLoanBySeqInputDto;
+import book.dto.BookDto.ExtendBookLoanBySeqOutputDto;
+import book.dto.BookDto.ManageBookRequestInputDto;
+import book.dto.BookDto.ManageBookRequestOutputDto;
+import book.dto.BookDto.RateBookInputDto;
+import book.dto.BookDto.RecommendBookInputDto;
+import book.dto.BookDto.RecommendBookOutputDto;
+import book.dto.BookDto.RegisterBookRequestedInputDto;
+import book.dto.BookDto.RegisterBookUnrequestedInputDto;
+import book.dto.BookDto.RequestBookInputDto;
+import book.dto.BookDto.ReturnBookBySeqInputDto;
+import book.dto.BookDto.ReturnBookBySeqOutputDto;
+import book.dto.BookDto.SearchBookByCategoryInputDto;
+import book.dto.BookDto.SearchBookByCategoryOutputDto;
+import book.dto.BookDto.SearchBookBySeqInputDto;
+import book.dto.BookDto.SearchBookBySeqOutputDto;
+import book.dto.BookDto.SearchBookByTitleInputDto;
+import book.dto.BookDto.SearchBookByTitleOutputDto;
+import book.dto.BookDto.UpdateBookBySeqInputDto;
+import book.dto.BookDto.ViewBookRequestsInputDto;
+import book.dto.BookDto.ViewBookRequestsOutputDto;
+import book.dto.BookDto.ViewFavoriteBookInputDto;
+import book.dto.BookDto.ViewFavoriteBookOutputDto;
+import book.dto.BookDto.ViewLoanHistoryInputDto;
+import book.dto.BookDto.ViewLoanHistoryOutputDto;
 
 public interface BookService {
 	
-	SearchBookByTitleOutput searchBookByTitle(SearchBookByTitleInput er);
+	List<SearchBookByTitleOutputDto> searchBookByTitle(SearchBookByTitleInputDto ip);
+	
+	List<SearchBookByCategoryOutputDto> SearchBookByCategory(SearchBookByCategoryInputDto ip);
+	
+	SearchBookBySeqOutputDto SearchBookBySeq(SearchBookBySeqInputDto ip);
+	
+	void AddFavoriteBook(AddFavoriteBookInputDto ip);
+	
+	List<ViewFavoriteBookOutputDto> ViewFavoriteBook(ViewFavoriteBookInputDto ip);
+	
+	void DeleteFavoriteBook(DeleteFavoriteBookInputDto ip);
+	
+	CheckBookAvailabilityBySeqOutputDto CheckBookAvailabilityBySeq(CheckBookAvailabilityBySeqInputDto ip);
+	
+	void RegisterBookUnrequested(RegisterBookUnrequestedInputDto ip);
+	
+	void RegisterBookRequested(RegisterBookRequestedInputDto ip);
+	
+	void UpdateBookBySeq(UpdateBookBySeqInputDto ip);
+	
+	void DeleteBook(DeleteBookInputDto ip);
+	
+	void RequestBook(RequestBookInputDto ip);
+	
+	ViewBookRequestsOutputDto ViewBookRequests(ViewBookRequestsInputDto ip);
+	
+	ManageBookRequestOutputDto ManageBookRequest(ManageBookRequestInputDto ip);
+	
+	ReturnBookBySeqOutputDto ReturnBookBySeq(ReturnBookBySeqInputDto ip);
+	
+	ExtendBookLoanBySeqOutputDto ExtendBookLoanBySeq(ExtendBookLoanBySeqInputDto ip);
+	
+	void RateBook(RateBookInputDto ip);
+	
+	List<ViewLoanHistoryOutputDto> ViewLoanHistory(ViewLoanHistoryInputDto ip);
+	
+	List<RecommendBookOutputDto> RecommendBook(RecommendBookInputDto ip);
 	
 }
