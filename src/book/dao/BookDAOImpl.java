1073d34045b6f913150fa095cd91e9b7b220377f
UserJiwon <jwngo@naver.com>
2024-03-05 17:43:08 +0900

    0305 BookDAOImpl 수정

--- src/book/dao/BookDAOImpl.java
+++ src/book/dao/BookDAOImpl.java
@@ -12,33 +12,6 @@ import config.AppConfig;
 import config.AppConfig.DBUtil;
 
 public class BookDAOImpl implements BookDAO{
-	
-	public List<Book> SearchBook() throws SQLException{
-		Connection con = null;
-		Statement st = null;
-		ResultSet rs = null;
-		List<Book> booklist = new ArrayList<>();
-		String sql = "select * from 책";
-		
-		try { 
-			con = AppConfig.DBUtil.getConnection();
-			st = con.createStatement();
-			rs = st.executeQuery(sql);
-			
-			while(rs.next()) {
-				Book book = new Book(rs.getLong(1),rs.getString(2),rs.getString(3),
-						rs.getString(4),rs.getString(5),rs.getString(6),
-						rs.getInt(7),rs.getDate(8),rs.getString(9));
-				booklist.add(book);
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}finally {
-			DBUtil.DbClose(con, st, rs);
-		}
-		
-		return booklist;
-	}
 
 	@Override
 	public List<Book> findAllBook() {
