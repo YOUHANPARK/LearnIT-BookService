46bfd57e0d4a44409f9b96d98421760f1e2489ad
YOUHAN PARK <youhanbark@gmail.com>
2024-03-07 09:17:02 +0900

    추가 : class MemberController

--- src/member/controller/MemberController.java
+++ src/member/controller/MemberController.java
@@ -1,5 +1,21 @@
 package member.controller;
 
+import book.service.BookService;
+import member.dto.MemberDto;
+import member.dto.MemberDto.LoginMemberInputDto;
+import member.dto.MemberDto.LoginMemberOutputDto;
+import member.service.MemberService;
+
 public class MemberController {
+	static BookService bookService;
+	static MemberService memberService;
+	
+	/**
+	 * 로그인
+	 */
+	public static void login(String id, int password) {
+		
+		LoginMemberInputDto member = memberService.loginMember(id, password);
+	}
 
 }
